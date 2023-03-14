68d5d5124a58b3abb9b083d6177fa27712cbba8b
Butter3ean <dhughes11438@gmail.com>
2023-03-14 12:46:03 -0400

    Initial commit

---
+++ src/main/java/com/example/real_estate_listings_api/RealEstateListingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_estate_listings_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateListingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateListingsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real_estate_listings_api/RealEstateListingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_estate_listings_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateListingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
