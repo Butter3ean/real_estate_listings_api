4e4995ffaa3848c4f7e4371e2c5e225d620bf225
Butter3ean <dhughes11438@gmail.com>
2023-03-18 03:10:44 -0400

    added new data to realEstatedb.mv.db, added new functions in RealEstateListingRepository.java, added new ways to query the api in the RealEstateListingsController.java

--- src/main/java/com/example/real_estate_listings_api/Controllers/RealEstateListingsController.java
+++ src/main/java/com/example/real_estate_listings_api/Controllers/RealEstateListingsController.java
@@ -3,50 +3,80 @@ package com.example.real_estate_listings_api.Controllers;
 import com.example.real_estate_listings_api.Entities.RealEstateListing;
 import com.example.real_estate_listings_api.Exceptions.ListingNotFoundException;
 import com.example.real_estate_listings_api.Repositories.RealEstateListingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/realestatelistings")
 public class RealEstateListingsController {
 
-    private final RealEstateListingRepository repository;
+    @Autowired
+    private RealEstateListingRepository repository;
 
-    RealEstateListingsController(RealEstateListingRepository repository) {
-        this.repository = repository;
-    }
-
-    @GetMapping("/realestatelistings")
+    //Returns all listings
+    @GetMapping()
     List<RealEstateListing> getAll() {
         return repository.findAll();
     }
 
-    @GetMapping("/realestatelistings/{id}")
+    //Returns all listings that match a certain city
+    @GetMapping(params = {"city"})
+    List<RealEstateListing> getByCity(@RequestParam String city) {
+        return repository.findByAddress_city(city);
+    }
+
+    //Returns a single listing with that mls number
+    @GetMapping(params = {"mlsNum"})
+    Optional<RealEstateListing> getListingByMls(Integer mlsNum) {
+        Optional<RealEstateListing> listing = repository.findByMlsNum(mlsNum);
+        if(listing.isEmpty()) {
+            throw new ListingNotFoundException(mlsNum.longValue());
+        }
+        return repository.findByMlsNum(mlsNum);
+    }
+
+    //returns all listings with that zipcode
+    @GetMapping(params = {"zipCode"})
+    List<RealEstateListing> getListingByZipCode(String zipCode) {
+        return repository.findByAddress_zipCode(zipCode);
+    }
+
+    //returns a single listing with that id
+    @GetMapping("/{id}")
     RealEstateListing getSingleListing(@PathVariable Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new ListingNotFoundException(id));
     }
 
-    @RequestMapping(value = "realEsateListings", params = "mlsNum", method = GET)
-    @ResponseBody
-    RealEstateListing getListingByMlsNum(@RequestParam Integer mlsNum) {
-        return repository.findByMlsNum(mlsNum);
-    }
-
-    @RequestMapping(value = "realEstateListings", params = "city", method = GET)
-    @ResponseBody
-    List<RealEstateListing> getListingByCity(@RequestParam String city) {
-        return repository.findByAddress_City(city);
-    }
-
-    @PostMapping("/realestatelistings")
+    //allows a user to add a new listing to the db
+    @PostMapping()
     RealEstateListing newRealEstateListing(@RequestBody RealEstateListing newRealEstateListing) {
         return repository.save(newRealEstateListing);
     }
 
-    @DeleteMapping("/realestatelistings/{id}")
+    @PutMapping("/{id}")
+    RealEstateListing replaceListing(@RequestBody RealEstateListing newRealEstateListing, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(listing -> {
+                    listing.setListingUrl(newRealEstateListing.getListingUrl());
+                    listing.setAddress(newRealEstateListing.getAddress());
+                    listing.setCoordinates(newRealEstateListing.getCoordinates());
+                    listing.setPrice(newRealEstateListing.getPrice());
+                    listing.setImgUrl(newRealEstateListing.getImgUrl());
+                    listing.setMlsNum(newRealEstateListing.getMlsNum());
+                    return repository.save(listing);
+                })
+                .orElseGet(() -> {
+                    newRealEstateListing.setId(id);
+                    return repository.save(newRealEstateListing);
+                });
+    }
+
+    //deletes a listing with that id from the database
+    @DeleteMapping("/{id}")
     void deleteListing(@PathVariable Long id) {
         repository.deleteById(id);
     }
--- src/main/java/com/example/real_estate_listings_api/Repositories/RealEstateListingRepository.java
+++ src/main/java/com/example/real_estate_listings_api/Repositories/RealEstateListingRepository.java
@@ -4,10 +4,13 @@ import com.example.real_estate_listings_api.Entities.RealEstateListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RealEstateListingRepository extends JpaRepository<RealEstateListing, Long> {
 
-    RealEstateListing findByMlsNum(Integer mlsNum);
+    Optional<RealEstateListing> findByMlsNum(Integer mlsNum);
 
-    List<RealEstateListing> findByAddress_City(String city);
+    List<RealEstateListing> findByAddress_city(String city);
+
+    List<RealEstateListing> findByAddress_zipCode(String zipCode);
 }
